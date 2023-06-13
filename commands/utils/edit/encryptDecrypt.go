6e3395378dc68351dd1d742b28004c2c063d79fe
shubhindia <shubhindia123@gmail.com>
2023-06-13 21:00:56 +0530

    drop unused function ⚰️
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- commands/utils/edit/encryptDecrypt.go
+++ commands/utils/edit/encryptDecrypt.go
@@ -1,17 +1,8 @@
 package edit
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-
 	staticProvider "github.com/shubhindia/crypt-core/providers/static"
 )
-
-func mdHashing(input string) string {
-	byteInput := []byte(input)
-	md5Hash := md5.Sum(byteInput)
-	return hex.EncodeToString(md5Hash[:]) // by referring to it as a string
-}
 
 func DecodeAndDecrypt(encoded string, keyPhrase string) []byte {
 	return staticProvider.DecodeAndDecrypt(encoded, keyPhrase)
