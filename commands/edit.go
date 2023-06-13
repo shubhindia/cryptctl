f2afb9b567833c4fadbe9973351174bfe68290f3
shubhindia <shubhindia123@gmail.com>
2023-06-13 12:06:56 +0530

    open DecryptedSecret in editor and handle edits ✨
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- commands/edit.go
+++ commands/edit.go
@@ -1,8 +1,11 @@
 package commands
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
+	"regexp"
 
 	"github.com/shubhindia/hcictl/common"
 	"github.com/urfave/cli/v2"
@@ -10,6 +13,8 @@ import (
 
 	editutils "github.com/shubhindia/hcictl/commands/utils/edit"
 )
+
+var whitespaceRegexp *regexp.Regexp
 
 func init() {
 
@@ -65,7 +70,25 @@ func init() {
 
 			decryptedSecret.Data = decryptedData
 
-			fmt.Printf("%+v", decryptedSecret)
+			// marshal into yaml
+			decrypted, err := yaml.Marshal(&decryptedSecret)
+			if err != nil {
+				return fmt.Errorf("error marshaling decryptedSecret %s", err.Error())
+			}
+
+			editedManitest, err := editObjects(decrypted)
+			if err != nil {
+				return fmt.Errorf("error editing objects %s", err.Error())
+			}
+
+			// unmarshal the edited yaml into decryptedSecrets again to encrypt the new secrets
+			var newDecryptedSecrets editutils.DecryptedSecret
+
+			err = yaml.Unmarshal(editedManitest, &newDecryptedSecrets)
+			if err != nil {
+				return fmt.Errorf("error unmarshaling file %s", err.Error())
+			}
+			fmt.Printf("%+v", newDecryptedSecrets)
 
 			return nil
 
@@ -73,4 +96,80 @@ func init() {
 	}
 
 	common.RegisterCommand(cliCmd)
+}
+
+func editObjects(data []byte) ([]byte, error) {
+	manifestBuf := bytes.Buffer{}
+	_, _ = manifestBuf.Write(data)
+
+	for {
+
+		// make the YAML to show in the editor.
+		editorBuf := bytes.Buffer{}
+		_, _ = manifestBuf.WriteTo(&editorBuf)
+		editorReader := bytes.NewReader(editorBuf.Bytes())
+
+		// open a temporary file.
+		tmpfile, err := os.CreateTemp("", ".*.yaml")
+		if err != nil {
+			return nil, fmt.Errorf("error making tempfile %s", err.Error())
+		}
+		defer tmpfile.Close()
+		defer os.Remove(tmpfile.Name())
+		_, _ = editorReader.WriteTo(tmpfile)
+		_ = tmpfile.Sync()
+
+		// show the editor.
+		err = runEditor(tmpfile.Name())
+		if err != nil {
+			return nil, fmt.Errorf("error running editor %s", err.Error())
+		}
+
+		// re-read the edited file.
+		afterTmpfile, err := os.Open(tmpfile.Name())
+		if err != nil {
+			return nil, fmt.Errorf("error re-opening tempfile %s %s", tmpfile.Name(), err)
+		}
+		defer afterTmpfile.Close()
+		afterBuf := bytes.Buffer{}
+		_, err = afterBuf.ReadFrom(afterTmpfile)
+		if err != nil {
+			return nil, fmt.Errorf("error reading tempfile %s %s", tmpfile.Name(), err)
+		}
+
+		// check if the file was edited at all.
+		if bytes.Equal(editorBuf.Bytes(), afterBuf.Bytes()) {
+			fmt.Println("Edit cancelled. No changes made")
+			os.Exit(0)
+		}
+
+		// return the edited bytes
+		return afterBuf.Bytes(), nil
+
+	}
+
+}
+
+func runEditor(filename string) error {
+	whitespaceRegexp = regexp.MustCompile(`\s+`)
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		return fmt.Errorf("nso $EDITOR set")
+	}
+
+	// deal with an editor that has options.
+	editorParts := whitespaceRegexp.Split(editor, -1)
+	executable := editorParts[0]
+	executable, _ = exec.LookPath(executable)
+
+	editorParts = append(editorParts, filename)
+	cmd := exec.Command(executable, editorParts[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error running editor %s", err)
+	}
+	return nil
 }
