0b8d44594a77fd437b181adfb7bc6e4297b0fa23
shubhindia <shubhindia123@gmail.com>
2023-08-21 17:07:14 +0530

    initial initCmd
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

---
+++ commands/init.go
@@ -0,0 +1,37 @@
+package commands
+
+import (
+	"github.com/shubhindia/cryptctl/commands/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	Namespace string
+	Provider  string
+)
+
+func init() {
+
+	initCmd.Flags().StringVarP(&Namespace, "namespace", "n", "", "namespace to use (required)")
+	initCmd.MarkFlagRequired("namespace")
+	initCmd.Flags().StringVarP(&Provider, "provider", "p", "", "provider to use (required)")
+	initCmd.MarkFlagRequired("provider")
+	rootCmd.AddCommand(initCmd)
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init [flags]",
+	Short: "init",
+	Long:  "Init initializes the encrypted-secrets CLI",
+
+	RunE: func(_ *cobra.Command, args []string) error {
+
+		switch Provider {
+		case "k8s":
+			return utils.InitK8s(Namespace)
+		}
+
+		return nil
+
+	},
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/shubhindia/encrypted-secrets v0.0.0-20230804103637-c1f0571cb373
 	github.com/spf13/cobra v1.7.0
+	k8s.io/api v0.27.2
 	k8s.io/apimachinery v0.27.2
 	k8s.io/client-go v0.27.2
 	sigs.k8s.io/yaml v1.3.0
@@ -48,7 +49,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.27.2 // indirect
 	k8s.io/klog/v2 v2.90.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f // indirect
 	k8s.io/utils v0.0.0-20230209194617-a36077c30491 // indirect
